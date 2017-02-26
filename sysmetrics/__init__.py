450c1563e482f75198809af6b881ffaf85cec81d
Tarek Ziade <tarek@ziade.org>
2017-02-26 22:46:17 +0100

    first shot at a system probe

---
+++ sysmetrics/__init__.py
@@ -0,0 +1,55 @@
+import psutil
+import asyncio
+import functools
+import os
+import signal
+import graypy
+import logging
+import json
+
+
+loop = asyncio.get_event_loop()
+logger = logging.getLogger('sysmetrics')
+
+
+def _exit(signame):
+    loop.stop()
+
+
+def _probe():
+    info = {}
+    info['cpu'] = {'percent': psutil.cpu_percent(interval=None)}
+    disk_counters = psutil.disk_io_counters()
+    disk_io = {'read_count': disk_counters.read_count,
+               'write_count': disk_counters.write_count,
+               'read_bytes': disk_counters.read_bytes,
+               'write_bytes': disk_counters.write_bytes}
+
+    info['disk'] = disk_io
+    memory = {}
+    vm = psutil.virtual_memory()
+    memory['total'] = vm.total
+    memory['available'] = vm.available
+    memory['used'] = vm.used
+    memory['free'] = vm.free
+    info['memory'] = memory
+
+    logger.info(json.dumps(info))
+    loop.call_later(1., _probe)
+
+
+
+for signame in ('SIGINT', 'SIGTERM'):
+    loop.add_signal_handler(getattr(signal, signame),
+                            functools.partial(_exit, signame))
+
+
+handler = graypy.GELFHandler('localhost', 12201)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+loop.call_later(1., _probe)
+
+try:
+    loop.run_forever()
+finally:
+    loop.close()
